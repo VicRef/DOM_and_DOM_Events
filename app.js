66263b8919e5c9f7d75a29b059fa6078aff1dcda
MA <victor@clarusway.com>
2022-02-08 06:03:10 +0300

    finished

--- app.js
+++ app.js
@@ -1,8 +1,118 @@
-const btnNavigate = document.getElementById("btnNavigate");
-const textareaDOMTracker = document.getElementById("textareaDOMTracker");
+// ACCESSING DOM ELEMENTS
+
+// console.log(window);
+// console.log(document);
 
 
-btnNavigate.addEventListener("click", () => {
-    console.log(textareaDOMTracker.value);
-    textareaDOMTracker.value = "";
-})
+// let btnMagic = document.getElementById("btnMagic");
+// console.log("here is our newly created var: ", btnMagic);
+
+
+// let linkForFullStack = document.getElementById("linkFullStack");
+// console.log("here is our newly created var: ", linkForFullStack);
+
+// const ourH3 = document.getElementById("h3TestArea");
+// console.log(ourH3);
+
+// const ourH3 = document.querySelector("#h3TestArea");
+// console.log(ourH3);
+
+// const myParas = document.getElementsByClassName("para");
+// console.log(myParas);
+
+// const divRight = document.getElementsByClassName("right");
+// console.log(divRight);
+
+// const divRightQS = document.querySelector(".right");
+// console.log(divRightQS);
+
+// const paras = document.getElementsByTagName("p");
+// console.log(paras);
+
+// const parasWithQS = document.querySelector("p");
+// console.log(parasWithQS);
+
+// const parasWithQSA = document.querySelectorAll("p");
+// console.log(parasWithQSA);
+
+// const elementWithName = document.getElementsByName("fname");
+// console.log(elementWithName);
+
+// const myFirstPara = document.getElementById("firstPara");
+// console.log(myFirstPara.innerHTML);
+// console.log(myFirstPara.innerText);
+
+
+// const divRightQS = document.querySelector(".right");
+// console.log(divRightQS);
+// console.log(divRightQS.getAttribute("class"));
+
+// divRightQS.setAttribute("class", "tempClass");
+
+// const paras = document.getElementsByTagName("p")[2];
+// console.log(paras);
+// console.log(paras.innerHTML);
+// console.log(paras.innerText);
+
+// paras.innerHTML = "here is a new para...";
+// paras.innerText = "here is a new para...";
+
+
+// const myLinks = document.getElementsByClassName("menu")[0];
+// console.log(myLinks);
+
+// const myNewLink = document.createElement("a");
+// myNewLink.innerText = "Back End";
+// console.log(myNewLink);
+
+// myLinks.appendChild(myNewLink);
+
+// myNewLink.setAttribute("class", "inputFirstName");
+
+
+// // 🔥🔥🔥🔥🔥 E-MAIL(lovercase => ryan.daniel@clarusway.com) CHALLENGE  🔥🔥🔥🔥🔥
+
+// const firstName = document.getElementById("fname");
+// const lastName = document.getElementById("lname");
+// const email = document.getElementById("email");
+
+// email.value = `${firstName.value}.${lastName.value}@clarusway.com`;
+
+// email.style.color = "Red";
+// email.style.fontSize = "24px";
+// email.style.backgroundColor = "lightblue";
+
+// css: font-size ===> js: fontSize
+
+// const contentMagicButton = document.getElementById("btnMagic");
+// console.log(contentMagicButton.innerHTML);
+
+// contentMagicButton.innerHTML = "Navigate to CW";
+// contentMagicButton.innerText = "Navigate to CW";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btnNavigate = document.getElementById("btnNavigate");
+// const textareaDOMTracker = document.getElementById("textareaDOMTracker");
+
+
+// btnNavigate.addEventListener("click", () => {
+//     console.log(textareaDOMTracker.value);
+//     textareaDOMTracker.value = "";
+// })
